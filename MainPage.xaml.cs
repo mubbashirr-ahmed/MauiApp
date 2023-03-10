d56fea7975b716303af397deb197e91f9f8532a9
Mubbashir Ahmed <mubbaahir41@gmail.com>
2023-03-10 15:18:57 +0500

    Second

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -22,7 +22,7 @@ public partial class MainPage : ContentPage
 		items.count = 4;
 		await databaseItems.saveItemAsync(items);
 		var cc = await databaseItems.GetTableItemsAsync();
-		var size = cc.Count;
+		var size = cc.Count + 1;
         CounterBtn.Text = $"Count: {size}";
         SemanticScreenReader.Announce(CounterBtn.Text);
    
